747cb1074bae1c5af683fff72d9952ab03aca930
Zaheer Ali <zaheercode09@gmail.com>
2026-02-15 05:38:46 +0500

    Assignment four

---
+++ Assignment-04/app.js
@@ -0,0 +1,71 @@
+console.log("Hello Assignment Four");
+
+
+// const user = {
+//     name: "Zaheer",
+//     greet: function () {
+//         console.log("Hello " + this.name);
+//     }
+// };
+// user.greet();
+// _______________________________02)
+// function introduce(city, country) {
+//     console.log(
+//         "My name is " + this.name +
+//         ", I am " + this.age +
+//         ", from " + city + ", " + country
+//     );
+// }
+
+// const person = {
+//     name: "Zaheer",
+//     age: 18
+// };
+
+// // call karna 
+// introduce.call(person, "Karachi", "Pakistan");
+// ____________________________________03
+// const user = {
+//   name: "Zaheer",
+//   greet: function () {
+//     console.log("Hello " + this.name);
+//   }
+// };
+// setTimeout(user.greet, 3000)
+
+// ----------------------
+// function counter() {
+//   let count = 0; 
+
+//   return function () {
+//     count++;
+//     console.log("Count:", count);
+//   };
+// }
+
+// const myCounter = counter();
+
+// myCounter(); //  1
+// myCounter(); //  2
+// myCounter(); //  3 result aai ga
+// ____________________________________
+// Task 3.2 and 3.3 sir ye simj nhi aya hai 
+// ______________________________
+// console.log("Start"); 
+
+// setTimeout(() => {
+//     console.log("Timeout 1"); 
+// }, 0);
+
+// console.log("Middle"); 
+
+// setTimeout(() => {
+//     console.log("Timeout 2");
+// }, 100);
+
+// console.log("End"); 
+
+
+// 4.2 karta hn
+
+// {___}
